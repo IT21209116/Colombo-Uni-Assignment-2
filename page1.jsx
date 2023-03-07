d2b069bdb1516687af19f53021a2732586b26b1b
IT21209116 <102014493+IT21209116@users.noreply.github.com>
2023-03-07 23:24:13 +0530

    Add files via upload

---
+++ page1.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "style.css"; 
+function App() {
+  return (
+    <div className="v1_4">
+      <div className="v1_5"></div>
+      <div className="v1_6"></div>
+      <div className="v1_7"></div>
+      <span className="v1_8">Rosara Dayaratne </span>
+      <span className="v1_9">Information systems undergraduate at University of colombo school of computing.</span>
+      <span className="v1_10">I am a hardworking and ambitious individual with a great interest in the field of Information and Communication Technology and Interactive Media Design. I am also a Public Speaker with an interest in social justice and a passion to empower and influence. Furthermore, I am very enthusiastic about Speech and Drama and Fashion Designing.</span>
+      <a href="file:///C:/Users/94771/Desktop/Portfolio/portfolio.html">
+        <span className="v1_25">Home</span>
+      </a>
+      <a href="file:///C:/Users/94771/Desktop/Portfolio/portfolio.html">
+        <span className="v1_26">Award and Certification</span>
+      </a>
+      <a href="file:///C:/Users/94771/Desktop/Portfolio/page4.html">
+        <span className="v1_27">Contact</span>
+      </a>
+      <a href="file:///C:/Users/94771/Desktop/Portfolio/page3.html">
+        <span className="v1_28">Work</span>
+      </a>
+      <a href="file:///C:/Users/94771/Desktop/Portfolio/page2.html">
+        <span className="v1_29">Education</span>
+      </a>
+      <a href="file:///C:/Users/94771/Desktop/Portfolio/page2.html">
+        <span className="v1_30">Experience</span>
+        <span className="v1_31">Education </span>
+      </a>
+    </div>
+  );
+}
+
+export default App;
